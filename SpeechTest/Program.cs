3c66c29311b77f8f851ff36240aab90fcf1ff3f3
Saito Tsutomu <tsutomu@kke.co.jp>
2012-08-22 16:42:36 +0900

    Create

---
+++ SpeechTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using System.Speech.Synthesis;
+
+namespace SpeechTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+var syn = new System.Speech.Synthesis.SpeechSynthesizer();
+syn.SelectVoice("Microsoft Server Speech Text to Speech Voice (ja-JP, Haruka)");
+syn.Speak("こんにちは");
+		}
+	}
+}
